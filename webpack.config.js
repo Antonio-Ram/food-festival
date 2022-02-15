43e09b6ca9a9c47c1e417b2335397d1375797732
Antonio <tonito@umich.edu>
2022-02-15 13:04:48 -0500

    Trashing branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-require('bootstrap');
+//require('bootstrap');
 const path = require('path');
 const  webpack  = require('webpack');
 
 module.exports = {
     entry: './assets/js/script.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname + '/dist'),
         filename: 'main.bundle.js'
     },
     plugins:[
